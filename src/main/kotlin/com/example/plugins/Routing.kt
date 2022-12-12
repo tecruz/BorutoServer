70e2c83b69f8a848363e7cd7ed90925a933ee82b
62000452 <tiago.cruz@gls-portugal.com>
2022-12-12 14:39:59 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,23 @@
+package com.example.plugins
+
+import com.example.routes.getAllHeroes
+import com.example.routes.root
+import com.example.routes.searchHeroes
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+    routing {
+        root()
+        getAllHeroes()
+        searchHeroes()
+
+        static("/images") {
+            resources("images")
+        }
+    }
+}
